b5d43f0a780c65cf89ad9002c4eb45fa58aa91c2
Antonio Amaya <antonio.amaya@gmail.com>
2024-04-26 09:25:30 +0000

    Added questions, answers and correct answers

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+questions = [
+{
+   "prompt": "What is the meaning of the word Guatemala?",
+   "options": ["A. Place of many trees", "B. Place of many rivers ", "C. Place of many mountains", "D. Place of many rocks"],
+   "answer": "A"
+},
+{
+   "prompt": "How many volcanoes there are in Guatemala?",
+   "options": ["A. 35", "B. 36 ", "C. 37", "D. 38"],
+   "answer": "C"
+},
+{
+   "prompt": "What is Guatemala's national bird?",
+   "options": ["A. Vireonidae", "B. Antbird", "C. Gnatcatcher", "D. Quetzal"],
+   "answer": "D"
+},
+{
+   "prompt": "What is the name of Guatemala's president?",
+   "options": ["A.  Juan José Arévalo", "B.  Bernardo Arévalo ", "C.  Alejandro Giammattei", "D. Alejandro Maldonado"],
+   "answer": "B"
+},
+{
+   "prompt": "When did Guatemala declare its independence from the Spanish Empire?",
+   "options": ["A. 1 July 1823", "B. 17 April 1839", "C. 31 May 1985", "D. 15 September 1821"],
+   "answer": "D"
+},
+{
+   "prompt": "How many states does Guatemala have?",
+   "options": ["A. 22", "B. 23", "C.  24", "D. 25"],
+   "answer": "A"
+},
+]
+
